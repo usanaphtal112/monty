7d45614a15eef8db81d1243ff8fb61b14108ccb2
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2023-12-14 11:06:46 -0800

    Added test files and run

--- monty.h
+++ monty.h
@@ -1,5 +1,6 @@
 #ifndef MONTY_H
 #define MONTY_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -9,26 +10,34 @@
 #include <ctype.h>
 
 /**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
  *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
  */
-typedef struct instruction_s
+typedef struct stack_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
-
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
 /**
- * struct bus_s - variables -args, file, line content
- * @arg: value
- * @file: pointer to monty file
- * @content: line content
- * @lifi: flag change stack <-> queue
- * Description: carries values through the program
+* @struct bus_s
+ * @brief Structure representing a bus with specific properties.
+ * @var bus_s::arg
+ * Member variable representing command-line arguments associated with the bus
+ * @var bus_s::file
+ * Member variable representing a file pointer associated with the bus.
+ * @var bus_s::content
+ * Member variable representing content associated with the bus.
+ * @var bus_s::lifi
+ * Member variable representing an integer value associated with the bus.
+ *
+ * @var bus
+ * External instance of the 'bus_t' structure that can be used globally.
  */
 typedef struct bus_s
 {
@@ -36,24 +45,23 @@ typedef struct bus_s
 	FILE *file;
 	char *content;
 	int lifi;
-}  bus_t;
+} bus_t;
 extern bus_t bus;
 
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
  *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
  */
-typedef struct stack_s
+typedef struct instruction_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
-} stack_t;
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
 
 void f_mul(stack_t **head, unsigned int counter);
 int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
@@ -78,6 +86,7 @@ void f_sub(stack_t **head, unsigned int counter);
 void f_stack(stack_t **head, unsigned int counter);
 void f_pchar(stack_t **head, unsigned int counter);
 
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 void f_pstr(stack_t **head, unsigned int counter);
 void f_rotl(stack_t **head, unsigned int counter);
 void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
